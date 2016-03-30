1b67f55e6e33ac09c23a74a542265ee5d9969577
JBorrell <boz_012@hotmail.com>
2016-03-30 14:28:51 +0100

    Adds tests to controller

--- js/iPlayerProgrammesController.js
+++ js/iPlayerProgrammesController.js
@@ -1,3 +1,17 @@
 iPlayerProgrammes.controller('iPlayerProgrammesController', [function() {
+  var self = this;
+
+  self.searchResult = {
+    "list": [
+      {
+        "title": "Vikings",
+        "avatar": "vikings.png"
+      },
+      {
+        "title": "Black Sails",
+        "avatar": "BlackSails.png"
+      }
+    ]
+  };
   
-}])
+}]);
--- test/iPlayerProgrammesController.spec.js
+++ test/iPlayerProgrammesController.spec.js
@@ -6,8 +6,25 @@ describe('iPlayerProgrammesController', function() {
   beforeEach(inject(function($controller) {
     ctrl = $controller('iPlayerProgrammesController');
   }));
-});
 
-it('initializes with an empty program result list', function() {
-  expect(ctrl.list).toBeUndefined();
+  it('initializes with an empty program result list', function() {
+    expect(ctrl.list).toBeUndefined();
+  });
+
+  describe('listing programmes', function() {
+    var list = [
+      {
+        "title": "Vikings",
+        "avatar": "vikings.png"
+      },
+      {
+        "title": "Black Sails",
+        "avatar": "BlackSails.png"
+      }
+    ];
+    it('displays list of avalable programmes', function(){
+      expect(ctrl.searchResult.list).toEqual(list)
+    });
+  });
+  
 });
