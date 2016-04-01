6ddd6b57288ba899b2d460effee7897b3a68737e
JBorrell <boz_012@hotmail.com>
2016-04-01 16:07:23 +0100

    Updates readme, refactors html

--- README.md
+++ README.md
@@ -4,3 +4,40 @@ Web application the displays programs available to watch in a list format.
 List will display title and image of programs and can be sorted according to
 the first letter of the program. Results for a single letter will be
 displayed over multiple pages to ease readability.
+
+Installation Instructions
+=========
+##### Setup #####
+1. Clone this repo to your local machine
+2. Navigate to home folder and ensure node & bower are installed
+3. Run "npm install" in your terminal
+4. Run "open index.html"
+5. Use site as desired
+
+##### Testing #####
+
+Karma
+1. Navigate to the home directory
+2. Run "karma start test/karma.conf.js" in your terminal
+
+Protractor:
+
+3 Separate Tabs in Terminal, make sure you're in the home directory
+
+- Tab 1: Run "http-server"
+- Tab 2: Run "webdriver-manager start"
+- Tab 3: Run "protractor test/e2e/conf.js"
+
+
+Technologies
+========
+- Angular
+- Karma
+- Protractor
+- Jasmine
+- HTML5
+- CSS
+
+Author
+======
+James Borrell [LinkedIn](https://www.linkedin.com/in/james-borrell-2281b1116)
--- test/e2e/iPlayerProgrammesListFeature.js
+++ test/e2e/iPlayerProgrammesListFeature.js
@@ -3,44 +3,41 @@ describe('BBC iPlayer', function(){
   pages = element.all(by.repeater('page in iPlayerCtrl.pages'));
   shows = element.all(by.repeater('prog in iPlayerCtrl.searchResult'));
 
+  beforeEach(function(){
+    browser.get('http://localhost:8080/')
+  });
+
   it('has a title', function(){
-    browser.get('http://localhost:8080/');
     expect(browser.getTitle()).toEqual('iPlayer Programmes');
   });
 
   it('has a logo/title', function(){
-    browser.get('http://localhost:8080/');
     image = element(by.id('logo')).getAttribute('src');
     expect(image).toEqual("http://localhost:8080/visuals/images/BBC_iPlayer_logo.svg");
   });
 
   it('able to browse all letter of the alphabet & 0-9', function(){
-    browser.get('http://localhost:8080/');
     expect(letters.count()).toEqual(27);
   });
 
   it('starts with the letter a', function(){
-    browser.get('http://localhost:8080/');
     expect(letters.first().getText()).toEqual('A');
   });
 
   it('can navigate to different letters', function(){
-    browser.get('http://localhost:8080/');
     expect(shows.first().getText()).toContain('Abadas');
     letters.get(7).click();
     expect(shows.first().getText()).toContain('Hacio');
   });
 
   it('results have paginated functionality', function(){
-    browser.get('http://localhost:8080/');
     expect(pages.count()).toEqual(4);
     expect(element(by.id('programmes')).getText()).toContain('Page:');
     pages.get(3).click();
-    expect(shows.first().getText()).toContain('The Arts Show');
+    expect(shows.first().getText()).toContain('Artsnight');
   })
 
   it('hides page navigation if only 1 page of results', function(){
-    browser.get('http://localhost:8080/');
     letters.get(26).click();
     expect(element(by.id('pages')).isDisplayed()).toBeFalsy;
   })
